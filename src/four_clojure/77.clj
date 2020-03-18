985502724055ae659843fe34e03c80bd4429f239
Andrey Miskov <amiskov@gmail.com>
2020-03-18 19:45:00 +0300

    Fix typo

--- src/four_clojure/77.clj
+++ src/four_clojure/77.clj
@@ -12,7 +12,7 @@
 
 ; Group words by sorted sequence of their letters (key is the sorted sequence of letters, value is the vector of
 ; words composed from those letters).
-; Than take those vectors of words and make sets from them.
+; Then take those vectors of words and make sets from them.
 ; Remove sets with no anagram (consists of only one word).
 ; Make a final set of sets.
 (defn anagram-finder [words]
