19e89ff8bd571ba1e559ab8307fb8680bf6eb204
Andrey Miskov <amiskov@gmail.com>
2020-03-20 07:04:04 +0300

    New problems

---
+++ src/four_clojure/162.clj
@@ -0,0 +1,11 @@
+(ns four-clojure.162
+  (:require [clojure.test :as t]))
+
+;; Logical falsity and truth
+;; Difficulty:	Elementary
+;; Topics:	    logic
+;;
+;; In Clojure, only nil and false represent the values of logical falsity in conditional tests -
+;; anything else is logical truth.
+
+1
---
+++ src/four_clojure/38.clj
@@ -0,0 +1,17 @@
+(ns four-clojure.38
+  (:require [clojure.test :as t]))
+
+;; Maximum value
+;; Difficulty:	Easy
+;; Topics:	    core-functions
+;;
+;; Write a function which takes a variable number of parameters and returns the maximum value.
+
+(defn max-val [& args]
+  (last (sort args)))
+
+(t/testing "max value"
+  (t/is (= (max-val 1 8 3 4) 8))
+  (t/is (= (max-val 30 20) 30))
+  (t/is (= (max-val 45 67 11) 67)))
+
